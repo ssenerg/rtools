364b65d811d5981b86af7ef708166ac17efb835e
ssenerg <amirhossein.pdn@gmail.com>
2026-07-19 21:36:39 +0330

    refac: move read_stdin_or_file to utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,6 @@
 use arboard::Clipboard;
+use std::fs;
+use std::io::{self, Read};
 
 pub fn emit(text: &str, copy: bool) -> Result<(), String> {
     if text.is_empty() {
@@ -16,4 +18,16 @@ fn copy_to_clipboard(text: &str) -> Result<(), String> {
     clipboard
         .set_text(text.to_string())
         .map_err(|e| format!("failed to copy to clipboard: {e}"))
+}
+
+pub fn read_stdin_or_file(input_file: &Option<String>) -> Result<String, String> {
+    if let Some(file) = input_file {
+        fs::read_to_string(file).map_err(|e| format!("Failed to read from file: {}", e))
+    } else {
+        let mut buffer = String::new();
+        io::stdin()
+            .read_to_string(&mut buffer)
+            .map_err(|e| format!("Failed to read from stdin: {}", e))?;
+        Ok(buffer)
+    }
 }
